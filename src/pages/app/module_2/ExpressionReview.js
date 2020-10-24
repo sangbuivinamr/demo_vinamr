e6767104e47ad7527820dc785c0ea399e1352b49
Dev03-vinamr <73330185+Dev03-vinamr@users.noreply.github.com>
2020-10-24 20:33:43 +0700

    Styling the expression review

--- src/pages/app/module_2/ExpressionReview.js
+++ src/pages/app/module_2/ExpressionReview.js
@@ -6,7 +6,7 @@ import { EXPRESSION_REVIEW_DATA } from "../../../data/testing-data";
 
 const ExpressionReview = (props) => {
     const renderExpressionHeader = () => {
-        let headerElement = ['code', 'Description'];
+        let headerElement = ['Code', 'Description'];
         return headerElement.map( (key, index) => {
             return <th key={index}> {key}</th>
         })
@@ -17,22 +17,22 @@ const ExpressionReview = (props) => {
            return (
                <tbody>
                  <tr key = {slide_index}>
-               <td>S{slide_index}</td>
+               <td className ="slide--index">S{slide_index}</td>
                <td>{slide_question}</td> 
            </tr>
            <tr>
             <td> </td>
-           <td>{slide_note}</td>
+           <td className ="slide--note">{slide_note}</td>
            </tr> 
            {slide_options.map(({option_index, option_description}) =>
            {
                return(
                    <tr key = {option_index}>
-                       <td>{option_index}</td>
+                       <td className ="option--index">{option_index}</td>
                        <td>
-                           <ul>
+                           
                <li>{option_description}</li>
-                           </ul>
+                        
                        </td>
                    </tr>
                )
@@ -47,16 +47,18 @@ const ExpressionReview = (props) => {
        }
 
 return(
-    <table>
+    <div className="quota-management-page--expression-review">
+    <table className="quota-management-page--expression-review--tables">
         <thead>
         {renderExpressionHeader()}
         </thead>
-      
+        
         {renderExpressionBody()}
         
       
         
     </table>
+    </div>
 )
 }
 export default ExpressionReview;
