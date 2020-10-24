b3f2489b43b56a4685bac1ba9ca25100b98d80b7
Dev02-vinamr <dev02@amr.com.vn>
2020-10-24 13:53:37 +0700

    Make the default bar is a component to reusable Fixed the style of the screen

--- src/pages/app/module_2/QuotaManagement.js
+++ src/pages/app/module_2/QuotaManagement.js
@@ -10,12 +10,8 @@
 
 //Packages
 import React from "react";
-import { IoIosArrowRoundUp } from "react-icons/io";
-import {IoIosArrowRoundDown} from "react-icons/io";
-import {IoMdClose} from "react-icons/io";
-import {IoIosSave} from "react-icons/io";
 import QuotaOverview from "./QuotaOverview";
-
+import DefaultBar from "../../../components/DefaultBar";
 
 //Styles
 import "./styles/QuotaManagementStyles.css";
@@ -24,57 +20,10 @@ import "./styles/QuotaManagementStyles.css";
 const QuotaManagement = (props)=>{
     return(
         <div className="quota-page">
-            <div className="quota-page default-bar">
-                <h2 className="h2">
-                    QUOTA SETTINGS
-                </h2>
-                <div className="up">
-                    <i>
-                    <IoIosArrowRoundUp
-                        className="up icon"
-                    />
-                    </i>
-                </div>
-                <div className="up">
-                    <i>
-                    <IoIosArrowRoundDown
-                        className="up icon"
-                    />
-                    </i>
-                </div>
-                <div className="up">
-                    <i>
-                    <IoMdClose
-                        className="up icon"
-                    />
-                    </i>
-                </div>
-                <div className="up">
-                    <i>
-                    <IoIosSave
-                        className="up icon"
-                    />
-                    </i>
-                </div>
-                <div className="mode">
-                    Mode:
-                </div>
-                    <select className="select">
-                        <option value="Expression"> Expression </option>
-                        <option value="Editing"> Editing </option>
-                        <option value="When Exceeded"> When Exceeded </option>
-                        <option value="Tracking"> Tracking </option>
-                    </select>
-                <div className="expression-review">
-                    <h2 className="review">
-                        EXPRESSION REVIEW
-                    </h2>
-                    <p>Silde 05/334</p>
-                </div>
-            </div>
+            <DefaultBar/>    
             <div className="quota-page--tables">
                 <QuotaOverview/>
-                <QuotaOverview/>
+                {/* <QuotaOverview/> */}
             </div>
         </div>
     );
