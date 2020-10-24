b95a2e3132cafc6ab2a66f72990232c601182096
Dev02-vinamr <dev02@amr.com.vn>
2020-10-24 09:20:17 +0700

    Finish the main UI of Expression-model without Layout

--- src/pages/app/module_2/QuotaManagement.js
+++ src/pages/app/module_2/QuotaManagement.js
@@ -7,7 +7,13 @@
 *Function: Render Screen Quota Management
 
 */
+
+//Packages
 import React from "react";
+import { IoIosArrowRoundUp } from "react-icons/io";
+import {IoIosArrowRoundDown} from "react-icons/io";
+import {IoMdClose} from "react-icons/io";
+import {IoIosSave} from "react-icons/io";
 
 
 //Styles
@@ -16,22 +22,57 @@ import "./styles/QuotaManagementStyles.css";
 
 const QuotaManagement = (props)=>{
     return(
-            <div className="quota-page">
-                <h2 className="h2">
+        <div className="quota-page">
+            <div className="quota-page default-bar">
+            <h2 className="h2">
                     QUOTA SETTINGS
                 </h2>
-                <p> UP</p>
-                <p> down</p>
-                <p> delete</p>
-                <p>SAVE</p>
-                <select
-                    className="select"
-                >
-                    <option>
-
-                    </option>
-                </select>
+                <div className="up">
+                    <i>
+                    <IoIosArrowRoundUp
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoIosArrowRoundDown
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoMdClose
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoIosSave
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="mode">
+                    Mode:
+                </div>
+                    <select className="select">
+                        <option value="Expression"> Expression </option>
+                        <option value="Editing"> Editing </option>
+                        <option value="When Exceeded"> When Exceeded </option>
+                        <option value="Tracking"> Tracking </option>
+                    </select>
+                <div className="expression-review">
+                    <h2 className="review">
+                        EXPRESSION REVIEW
+                    </h2>
+                    <p>Silde 05/334</p>
+                </div>
             </div>
+                
+        </div>
     );
 };
 export default QuotaManagement;
