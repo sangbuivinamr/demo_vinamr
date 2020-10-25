af18ca029e57502a58f52b74b130606e5cf46996
unknown <dev01@amr.com.vn>
2020-10-25 14:02:29 +0700

    Fix naming in testing-data.js

--- src/data/testing-data.js
+++ src/data/testing-data.js
@@ -2,28 +2,28 @@ export const QUOTA_OVERVIEW_DATA = [
     {
         quota_index: 0,
         quota_label: "Ha Noi",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },
     {
         quota_index: 1,
         quota_label: "HCM",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },{
         quota_index: 2,
         quota_label: "Hai Phong",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },{
         quota_index: 3,
         quota_label: "Hue",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },{
         quota_index: 4,
         quota_label: "Ha Tinh",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },{
         quota_index: 5,
         quota_label: "Da Nang",
-        expression: "S6=1"
+        quota_expression: "S6=1"
     },
 ]
 
