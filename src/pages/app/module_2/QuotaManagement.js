26d1711030797ae4c4c142b5e296ebabef8a8252
Dev02-vinamr <dev02@amr.com.vn>
2020-10-25 09:39:46 +0700

    edited the file to handle

--- src/pages/app/module_2/QuotaManagement.js
+++ src/pages/app/module_2/QuotaManagement.js
@@ -5,10 +5,16 @@
 */
 
 //Packages
-import React from "react";
+import React,{useState}  from "react";
 import QuotaOverview from "./QuotaOverview";
-import DefaultBar from "../../../components/DefaultBar";
 import ExpressionReview from "./ExpressionReview";
+import { IoIosArrowRoundUp } from "react-icons/io";
+import {IoIosArrowRoundDown} from "react-icons/io";
+import {IoMdClose} from "react-icons/io";
+import {IoIosSave} from "react-icons/io";
+import axios from 'axios';
+
+
 //Styles
 import "./styles/QuotaManagementStyles.css";
 
@@ -16,7 +22,54 @@ import "./styles/QuotaManagementStyles.css";
 const QuotaManagement = (props)=>{
     return(
         <div className="quota-page">
-            <DefaultBar/>    
+           <div className="quota-page default-bar">
+                <h2 className="h2-default">
+                    QUOTA SETTINGS
+                </h2>
+                <div className="up">
+                    <i>
+                    <IoIosArrowRoundUp
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoIosArrowRoundDown
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoMdClose
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="up">
+                    <i>
+                    <IoIosSave
+                        className="up icon"
+                    />
+                    </i>
+                </div>
+                <div className="mode">
+                    Mode:
+                </div>
+                    <select className="select">
+                        <option value="Expression"> Expression </option>
+                        <option value="Editing"> Editing </option>
+                        <option value="When Exceeded"> When Exceeded </option>
+                        <option value="Tracking"> Tracking </option>
+                    </select>
+                <div className="expression-review">
+                    <h2 className="review">
+                        EXPRESSION REVIEW
+                    </h2>
+                    <p>Silde 05/334</p>
+                </div>
+            </div>  
             <div className="quota-page--tables">
                 <QuotaOverview/>
                 <ExpressionReview/>
