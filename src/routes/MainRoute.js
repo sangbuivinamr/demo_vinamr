afacefa7e2321efffbb08bce59ccd696d1cf2ebd
Dev02-vinamr <dev02@amr.com.vn>
2020-10-26 17:24:22 +0700

    add the path for QuotaEditing

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import QuotaManagement from '../pages/app/module_2/mode_epression/QuotaManagement';
-
+import QuotaEditing from "../pages/app/module_2/mode_editing/QuotaEdting";
 // react-router-dom
 
 const MainRoute = () => {
@@ -9,6 +9,7 @@ const MainRoute = () => {
     <Router basename = '/demo'>
         <Switch> 
             <Route exact path="/" component={QuotaManagement} />
+            <Route exact path="/editing" component={QuotaEditing} />
         </Switch>
     </Router>
   );
