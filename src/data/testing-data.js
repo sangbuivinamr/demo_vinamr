bc30b3613c07010a4aad15200f4bb5113db3899a
Dev03-vinamr <73330185+Dev03-vinamr@users.noreply.github.com>
2020-11-05 16:30:36 +0700

    Research cái bảng chưa ra, nên chiều nay chưa làm đc gì

--- src/data/testing-data.js
+++ src/data/testing-data.js
@@ -32,7 +32,7 @@ export const EXPRESSION_REVIEW_DATA = [
         slide_index: 1,
         slide_question: "Vui lòng cho biết thu nhập trung bình hàng tháng của hộ gia đình anh/ chị? (SA)",
         slide_note: "[Nếu Đáp viên từ chối trả lời, PVV không hỏi lại và chọn code 99]",
-        slide_options: [
+        slide_options: [  
             {
                 option_index: 1,
                 option_description: "Dưới 5.000.000 đ"
@@ -246,39 +246,7 @@ export const EXPRESSION_REVIEW_DATA = [
         ]
     }
 ]
-export const EXCEEDED_LAYOUT_LEFT = [
-    
-    {
-        type_index: 1,
-        type_car: "Owner. Honda Accord",
-        check:"Đã hết quota. Liên hệ F0 phụ trách"
-    },
-    {
-        type_index: 2,
-        type_car: "Owner. Toyota Camry",
-        check:"Đã hết quota. Liên hệ F1 phụ trách"
-    },
-    {
-        type_index: 3,
-        type_car: "Owner. Other Models",
-        check:"Đã hết quota. Liên hệ F2 phụ trách"
-    },
-    {
-        type_index: 4,
-        type_car: "Intend. Honda Accord",
-        check:"Đã hết quota. Liên hệ F3 phụ trách"
-    },
-    {
-        type_index: 5,
-        type_car: "Intend. Toyota Camry",
-        check:"Đã hết quota. Liên hệ F5 phụ trách"
-    },
-    {
-        type_index: 6,
-        type_car:"Intend. Other Models",
-        check:"Đã hết quota. Liên hệ F6 phụ trách"
-    },
-]
+
 export const EXCEEDED_SEX_LEFT=[
     {
         sex_index:0,
@@ -288,4 +256,82 @@ export const EXCEEDED_SEX_LEFT=[
         sex_index:1,
         exceeded_sex:"Nữ"
     }   
+]
+export const EXCEEDED_LAYOUT_LEFT = [
+    
+    {
+        type_index: 1,
+        type_car: "Owner. Honda Accord",
+        check:"Đã hết quota. Liên hệ F0 phụ trách",
+        data: [{place: "HA NOI", count:12},{place: "HA NOI", count:12},{place: "HA NOI", count:12}]
+    },
+    {
+        type_index: 2,
+        type_car: "Owner. Toyota Camry",
+        check:"Đã hết quota. Liên hệ F1 phụ trách",
+        quota_count: 30 
+    },
+    {
+        type_index: 3,
+        type_car: "Owner. Other Models",
+        check:"Đã hết quota. Liên hệ F2 phụ trách",
+        quota_count: 29 
+    },
+    {
+        type_index: 4,
+        type_car: "Intend. Honda Accord",
+        check:"Đã hết quota. Liên hệ F3 phụ trách",
+        quota_count: 26 
+    },
+    {
+        type_index: 5,
+        type_car: "Intend. Toyota Camry",
+        check:"Đã hết quota. Liên hệ F5 phụ trách",
+        quota_count: 45
+    },
+    {
+        type_index: 6,
+        type_car:"Intend. Other Models",
+        check:"Đã hết quota. Liên hệ F6 phụ trách",
+        quota_count: 23
+    },
+]
+export const EDITING_TABLE_DATA = [
+    
+    {
+        type_index: 1,
+        type_car: "Owner. Honda Accord",
+        check:"Đã hết quota. Liên hệ F0 phụ trách",
+        quotaData: [12,20, 30,26, 30,6]
+    },
+    {
+        type_index: 2,
+        type_car: "Owner. Toyota Camry",
+        check:"Đã hết quota. Liên hệ F1 phụ trách",
+        quotaData: [12,13, 38,26, 30,65]
+    },
+    {
+        type_index: 3,
+        type_car: "Owner. Other Models",
+        check:"Đã hết quota. Liên hệ F2 phụ trách",
+        quotaData: [12,13, 40,26, 30,12]
+    },
+    {
+        type_index: 4,
+        type_car: "Intend. Honda Accord",
+        check:"Đã hết quota. Liên hệ F3 phụ trách",
+        quotaData: [12,13, 30,26, 50,34]
+    },
+    {
+        type_index: 5,
+        type_car: "Intend. Toyota Camry",
+        check:"Đã hết quota. Liên hệ F5 phụ trách",
+        quotaData: [12,13, 30,86, 30,45]
+    },
+    {
+        type_index: 6,
+        type_car:"Intend. Other Models",
+        check:"Đã hết quota. Liên hệ F6 phụ trách",
+        quotaData: [12,13, 30,77, 30,42]
+    },
 ]
