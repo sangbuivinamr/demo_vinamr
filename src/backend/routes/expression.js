f7eb21e22a30482ff2a64898a2ec608b2f7a9d17
sangbuivinamr <71807588+sangbuivinamr@users.noreply.github.com>
2020-11-11 15:41:17 +0700

    Revert "Module 4 -> Module Expression Backend mispush by Hoan"

--- src/backend/routes/expression.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const expression = require('../controller/expression/expression_review')
-
-router.get('/expressionReview/', expression.getCodeExpression)
-
-module.exports = router
---
+++ src/pages/app/module_2/QuotaOverview.js
@@ -0,0 +1,55 @@
+
+import React, {useState, useEffect} from "react";
+
+
+//Styles
+import "./styles/QuotaOverview.css";
+import QuotaRow from "../../../components/app/QuotaRow";
+
+const QuotaOverview = (props)=>{
+
+    const renderHeader = () => {
+        let headerElement = ['Quota Label', 'Expression']
+
+        return headerElement.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+
+    return (
+        <>
+            <table id='quota-overview-table'>
+                <thead>
+                    <tr>{renderHeader()}</tr>
+                </thead>
+                <tbody className="quota-overview-table--body">
+                    <QuotaRow
+                        quotaData={props.quotaData}
+                        quotaClickStatus={props.quotaClickStatus}
+                        setQuotaClickStatus={props.setQuotaClickStatus}
+                        onChoosingQuota={props.onChoosingQuota}
+                    />
+                    <td>
+                        <input
+                            placeholder=""
+                            value={props.quotaInput.quota_label}
+                            onChange={(text) => props.setQuotaLabel(text.target.value)}
+                            className="quota-overview-table--input"
+                        />
+                    </td>
+                    <td>
+                        <input
+                            placeholder=""
+                            value={props.quotaInput.quota_expression}
+                            onChange={(text) => props.setQuotaExpression(text.target.value)}
+                            className="quota-overview-table--input"
+                        />
+                    </td>
+                    
+                    
+                </tbody>
+            </table>
+        </>
+    )
+}
+export default QuotaOverview;
