e1527cfe4786a23be99b22c65ab041db40270cb1
Dev02-vinamr <dev02@amr.com.vn>
2020-11-13 23:16:35 +0700

    Re-push

---
+++ src/pages/app/module_4/RawData.js
@@ -0,0 +1,21 @@
+/*
+* Contributor: 
+    - Tiến 13/11/2020
+*Main Function: render module_4
+*/
+
+//Packages
+import React from "react";
+
+
+//Styles
+import "./RawData.css";
+
+const RawData = (props) => {
+    return(
+        <div>
+            hi
+        </div>
+    )
+}
+export default RawData;
--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,22 +1,24 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import QuotaManagement from '../pages/app/module_2/mode_expression/QuotaManagement';
+import QuotaManagement from "../pages/app/module_2/mode_expression/QuotaManagement";
 import QuotaEditing from "../pages/app/module_2/mode_editing/QuotaEditing";
 import QuotaTracking from "../pages/app/module_2/mode_tracking/QuotaTracking";
 import QuotaExceeded from "../pages/app/module_2/mode_when_exceeded/QuotaExceeded";
 import QuotaInterview from "../pages/app/module_2/mode_interview/QuotaInterview";
+import RawData from "../pages/app/module_4/RawData";
 // react-router-dom
 
 const MainRoute = () => {
   return (
-    <Router basename = '/demo'>
-        <Switch> 
-            <Route exact path="/editing" component={QuotaEditing}/>
-            <Route exact path="/exceeded" component={QuotaExceeded}/>
-            <Route exact path="/tracking" component={QuotaTracking} />
-            <Route exact path="/interview" component={QuotaInterview} />
-            <Route exact path="/" component={QuotaManagement} />
-        </Switch>
+    <Router basename="/demo">
+      <Switch>
+          <Route exact path="/editing" component={QuotaEditing} />
+          <Route exact path="/exceeded" component={QuotaExceeded} />
+          <Route exact path="/tracking" component={QuotaTracking} />
+          <Route exact path="/interview" component={QuotaInterview} />
+          <Route exact path="/" component = {QuotaManagement}/>
+          <RawData exact path = "/rawData"/>
+      </Switch>
     </Router>
   );
 };
