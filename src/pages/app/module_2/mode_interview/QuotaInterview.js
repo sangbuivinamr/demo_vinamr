042c085afd63119314bb5e994a2001013fb50004
Dev02-vinamr <dev02@amr.com.vn>
2020-11-13 23:23:01 +0700

    Inherit the code change state of mode from module_2

--- src/pages/app/module_2/mode_interview/QuotaInterview.js
+++ src/pages/app/module_2/mode_interview/QuotaInterview.js
@@ -1,85 +1,53 @@
 //Packages
-import React from "react";
+import React, { useState } from "react";
 import InterviewRow from "./InterviewRow";
-import {EXCEEDED_LAYOUT_LEFT} from "../../../../data/testing-data";
+import { EXCEEDED_LAYOUT_LEFT } from "../../../../data/testing-data";
 //Styles
 import "./styles/QuotaInterview.css";
 
+const QuotaInteview = (props) => {
+  const [selectedInterview, setSelectedInterview] = useState("interview"); // Initialize the state, so when the user navigate to this mode, the mode will have the mode is interview in the option
+  const onChangeNav = (e) => {
+    props.history.push(`/${e.target.value}`);
+  };
+  const renderHeader = () => {
+    let headerElement = ["Name", "Completed"];
 
-const QuotaInteview = (props)=>{
+    return headerElement.map((key, index) => {
+      return <th key={index}>{key}</th>;
+    });
+  };
 
-    
-    const onChangeNav=(e)=>{  props.history.push(`/${e.target.value}`) }
-    const renderHeader = () => {
-        let headerElement = ['Name', 'Completed']
+  return (
+    <div className="interview">
+      <div className="interview interview-bar">
+        <h2 className="h2-interview">QUOTA SETTINGS</h2>
 
-        return headerElement.map((key, index) => {
-            return <th key={index}>{key}</th>
-        })
-    }
-   
-    return(
-        <div className="interview">
-            <div className="interview interview-bar">
-                <h2 className="h2-interview">
-                    QUOTA SETTINGS
-                    
-                </h2>
-               
-                <div className="mode-interview">
-                    Mode: 
-                </div>
-                    <select className="select" onChange={onChangeNav} /*value={}*/>
-                        <option
-                            value=""
-                        > 
-                            Expression 
-                        </option>
-                        <option 
-                            value="editing"
-                        >
-                            Editing 
-                        </option>
-                        <option
-                            value="exceeded"
-                        >
-                            When Exceeded 
-                        </option>
-                        <option 
-                            value="tracking"
-                        >
-                            Tracking
-                        </option>
-                        <option
-                            value="interview"
-                        >
-                            Interview Preview
-                        </option>
-                    </select>
-            </div>
-            <div id="prev">
-                    <table id="table-prev">
-                        <thead>
-                            <tr>{renderHeader()}</tr>
-                        </thead>
-                        <tbody>
-                            <InterviewRow
-                                exceededLeft={EXCEEDED_LAYOUT_LEFT}
-                            />
-                            <InterviewRow
-                                exceededLeft={EXCEEDED_LAYOUT_LEFT}
-                            />
-                            <InterviewRow
-                                exceededLeft={EXCEEDED_LAYOUT_LEFT}
-                            />
-
-                        </tbody>
-                    </table>
-                       
-
-            </div>
-        </div>
-    );
+        <div className="mode-interview">Mode:</div>
+        <select
+          className="select"
+          onChange={onChangeNav}
+          value={selectedInterview} //Initialize state for the mode when user naivgate to this mode
+        >
+          <option value="">Expression</option>
+          <option value="editing">Editing</option>
+          <option value="exceeded">When Exceeded</option>
+          <option value="tracking">Tracking</option>
+          <option value="interview">Interview Preview</option>
+        </select>
+      </div>
+      <div id="prev">
+        <table id="table-prev">
+          <thead>
+            <tr>{renderHeader()}</tr>
+          </thead>
+          <tbody>
+            <InterviewRow exceededLeft={EXCEEDED_LAYOUT_LEFT} />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default QuotaInteview;
--- src/pages/app/module_2/mode_tracking/QuotaTracking.js
+++ src/pages/app/module_2/mode_tracking/QuotaTracking.js
@@ -1,77 +1,58 @@
 //Packages
-import React, { useContext } from "react";
+import React, { useState } from "react";
 import { FaSyncAlt } from "react-icons/fa";
-import {MainContext} from "../../../../components/app/Context";
+
 //Styles
 import "./styles/QuotaTracking.css";
 
+const QuotaTracking = (props) => {
+  const [selectedTracking, setSelectedTracking] = useState("tracking");
 
-const QuotaTracking = (props)=>{
-    const onChangeNav=(e)=>{  props.history.push(`/${e.target.value}`) }
-    return(
-        <div className="tracking">
-            <div className="tracking tracking-bar">
-                <div className="bar">
-                    <h2 className="h2-tracking">
-                        QUOTA SETTINGS
-                    </h2>
-                    <div className="export area" >
-                    <div className="export">
-                        <p className="p-tracking">Export Quota</p>
-                    </div>
-                    <div className="mode-tracking">
-                        Mode: 
-                    </div>
-                        <select className="select" onChange={onChangeNav} /*value={}*/>
-                            <option
-                                value=""
-                            > 
-                                Expression 
-                            </option>
-                            <option 
-                                value="editing"
-                            >
-                                Editing 
-                            </option>
-                            <option
-                                value="exceeded"
-                            >
-                                When Exceeded 
-                            </option>
-                            <option 
-                                value="tracking"
-                            >
-                                Tracking
-                            </option>
-                            <option
-                                value="interview"
-                            >
-                                Interview Preview
-                            </option>
-                        </select>
-                        <div className="icon-sync">
-                            <i>
-                            <FaSyncAlt
-                                className="sync-icon"
-                            onClick={()=>console.log("nguasa")}
-                            />
-                        </i>
-                        </div>
-                        <p 
-                            // onChange=
-                            className="sync"
-                        >
-                            Last Update
-                        </p>
-                    </div>
-                </div>
-
-                <div className="content-tracking">
-                   
-                </div>
+  const onChangeNav = (e) => {
+    props.history.push(`/${e.target.value}`);
+  };
+  return (
+    <div className="tracking">
+      <div className="tracking tracking-bar">
+        <div className="bar">
+          <h2 className="h2-tracking">QUOTA SETTINGS</h2>
+          <div className="export area">
+            <div className="export">
+              <p className="p-tracking">Export Quota</p>
             </div>
+            <div className="mode-tracking">Mode:</div>
+            <select
+              className="select"
+              onChange={onChangeNav}
+              value={selectedTracking} //Initialize state for the mode when user naivgate to this mode
+            >
+              <option value="">Expression</option>
+              <option value="editing">Editing</option>
+              <option value="exceeded">When Exceeded</option>
+              <option value="tracking">Tracking</option>
+              <option value="interview">Interview Preview</option>
+            </select>
+            <div className="icon-sync">
+              <i>
+                <FaSyncAlt
+                  className="sync-icon"
+                  onClick={() => console.log("nguasa")}
+                />
+              </i>
+            </div>
+            <p
+              // onChange=
+              className="sync"
+            >
+              Last Update
+            </p>
+          </div>
         </div>
-    );
+
+        <div className="content-tracking"></div>
+      </div>
+    </div>
+  );
 };
 
 export default QuotaTracking;
