8dea5d5eb75a4ba5d133b4c00513f1f8b5ffc5aa
Tran Khai Hoan <hoan.tran@vinamr.local>
2020-11-16 13:18:19 +0700

    backend update editing model

--- src/backend/controller/quota/quota_editing.js
+++ src/backend/controller/quota/quota_editing.js
@@ -5,45 +5,49 @@ const sqlQuery = require('../../database/mysql/mysql_query')
 module.exports.getTable = async (req, res, next) => {
     let projectId = req.query.projectId
     try {
-        let connection = dbConnection()
-        let query = 'SELECT * FROM sys.quota_editing WHERE projectID="' + projectId + '"'
-        let result = sqlQuery(connection, query)
-        res.json
+        let response = {}
+        let connection = await dbConnection()
+        let query = "SELECT * FROM `sys`.`quota_editing` WHERE projectID=?"
+        let result = await sqlQuery(connection, query, [projectId])
+        console.log(result)
+        connection.end()
+        if (result.length == 0) {
+            return res.json({
+                message: "no projectID matching"
+            })
+        } else {
+            response.rowList = JSON.parse(result[0].rows)
+            response.colList = JSON.parse(result[0].columns)
+            response.data = JSON.parse(result[0].data)
+            return res.send(response)
+        }
     }
     catch (error) {
         console.log(error)
+        return res.json({
+            error: error
+        })
     }
 }
 
 module.exports.postTable = async (req, res) => {
     let projectId = req.query.projectId
     try {
-        let rowInput = {}
-        let colInput = {}
-        let dataInput = {}
-        let rowList = req.body.rowList
-        let colList = req.body.colList
-        let data = req.body.data
-        await rowList.forEach(a => rowInput[a] = a)
-        await colList.forEach(b => colInput[b] = b)
-        await data.forEach(c => dataInput[data.indexOf(c)] = c)
-        //chuyển qua string nhưng vẫn ko dc
-        x = JSON.stringify(rowInput)
-        y = JSON.stringify(colInput)
-        z = JSON.stringify(data)
-        console.log(rowInput, colInput, dataInput)
+        let rowInput = JSON.stringify(req.body.rowList)
+        let colInput = JSON.stringify(req.body.colList)
+        let dataInput = JSON.stringify(req.body.data)
         let connection = await dbConnection()
-        let query = 'INSERT INTO sys.quota_editing (projectID, rows, columns, data) VALUES ('+ projectId +', '+ rowInput +', '+ colInput +', '+ dataInput +')'
-        let result = await sqlQuery(connection, query)
+        let query = "INSERT INTO `sys`.`quota_editing` (`projectID`, `rows`, `columns`, `data`) VALUES (?, ?, ?, ?)"
+        let result = await sqlQuery(connection, query, [projectId, rowInput, colInput, dataInput])
         connection.end()
-        res.json({
-            message: "successfull"
+        res.status(200).json({
+            message: "insert successful"
         })
     }
     catch (error) {
-    console.log(error)
-    res.status(500).json({
-        error: error
-    })
-}
+        console.log(error)
+        res.status(500).json({
+            error: error
+        })
+    }
 }
