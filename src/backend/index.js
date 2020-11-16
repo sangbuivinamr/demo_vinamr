2112cfde9d471c0439bdb566e35dcfec64d3ab0b
Tran Khai Hoan <hoan.tran@vinamr.local>
2020-11-16 17:13:31 +0700

    backend update module 4

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,7 @@
 const quotaRoute = require('./routes/quota')
 const bodyParser = require('body-parser')
 const expressionRoute = require('./routes/expression')
+const dataCheckRoute = require('./routes/data_check')
 
 module.exports = function (app) {
     
@@ -8,6 +9,7 @@ module.exports = function (app) {
     app.use(bodyParser.json())
     app.use('/quota', quotaRoute)
     app.use('/expression',expressionRoute)
+    app.use('/dataCheck', dataCheckRoute)
 
     const PORT = process.env.PORT || 3000
 }
