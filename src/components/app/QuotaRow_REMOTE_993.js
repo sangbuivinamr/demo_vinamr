e8fcc034814e10cb21157cbd80b381da40e61d3c
sangbuivinamr <sang.bui@vinamr.com.vn>
2020-11-24 09:54:33 +0700

    Origin Files

--- src/components/app/QuotaRow_REMOTE_993.js
+++ src/components/app/QuotaRow_REMOTE_993.js
@@ -1,36 +1,25 @@
-/*
- * Contributor: 
-    Khánh
-    Tiến 10/11/2020 (Start hooking BE to FE)
- */
+import React, { useState } from 'react'
 
-//Packages
-import React from 'react';
 /**
  * @summary Render each row in the table
  * @param {object} props The props of QuotaRow component
  */
 export default function QuotaRow(props){
+
     // PROPS
     // quotaData: fixed data of the table
     // onChoosingQuota: onClick a row in the table
     // quotaClickStatus: status of the clicked row
 
-    return props.quotaData && props.quotaData.map(({ name, expression }) => {
+    return props.quotaData && props.quotaData.map(({ quota_label, quota_expression }) => {
         return (
             <tr
-<<<<<<< HEAD
-                key={name} 
-                onClick={() => props.onChoosingQuota(name)} 
-                style={{backgroundColor: props.quotaClickStatus.status && props.quotaClickStatus.quotaLabel === name && "#7B2025" }}
-=======
                 key={quota_label} 
                 onClick={() => props.onChoosingQuota(quota_label)} 
                 style={{backgroundColor: props.quotaClickStatus.status && props.quotaClickStatus.quotaLabel === quota_label && "#ffefcc" }}
->>>>>>> editing-model
             >
-                <td>{name}</td>
-                <td>{expression}</td>
+                <td>{quota_label}</td>
+                <td>{quota_expression}</td>
             </tr>
         )
     })
