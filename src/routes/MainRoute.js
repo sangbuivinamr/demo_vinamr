93d40de6704c6ac4582335098fb52aca555fce69
Dev02-vinamr <dev02@amr.com.vn>
2020-11-25 22:48:37 +0700

    Delete file module_2 in branch module_4

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,10 +1,5 @@
 import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import QuotaManagement from "../pages/app/module_2/mode_expression/QuotaManagement";
-import QuotaEditing from "../pages/app/module_2/mode_editing/QuotaEditing";
-import QuotaTracking from "../pages/app/module_2/mode_tracking/QuotaTracking";
-import QuotaExceeded from "../pages/app/module_2/mode_when_exceeded/QuotaExceeded";
-import QuotaInterview from "../pages/app/module_2/mode_interview/QuotaInterview";
 import RawData from "../pages/app/module_4/RawData";
 // react-router-dom
 
@@ -12,11 +7,6 @@ const MainRoute = () => {
   return (
     <Router basename="/demo">
       <Switch>
-          <Route exact path="/editing" component={QuotaEditing} />
-          <Route exact path="/exceeded" component={QuotaExceeded} />
-          <Route exact path="/tracking" component={QuotaTracking} />
-          <Route exact path="/interview" component={QuotaInterview} />
-          <Route exact path="/quotaManagement" component = {QuotaManagement}/>
           <Route exact path="/" component = {RawData}/>
       </Switch>
     </Router>
