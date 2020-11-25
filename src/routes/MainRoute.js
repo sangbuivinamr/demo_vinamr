649d87f37297313f2cee470edc3902d2430816a1
Dev02-vinamr <dev02@amr.com.vn>
2020-11-25 22:56:44 +0700

    Delete files irrelevant in branch when_exceeded

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -1,8 +1,6 @@
-import React,{useContext} from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import QuotaManagement from '../pages/app/module_2/mode_expression/QuotaManagement';
-import QuotaEditing from "../pages/app/module_2/mode_editing/QuotaEditing";
-import QuotaTracking from "../pages/app/module_2/mode_tracking/QuotaTracking";
 import QuotaExceeded from "../pages/app/module_2/mode_when_exceeded/QuotaExceeded";
 import QuotaInterview from "../pages/app/module_2/mode_interview/QuotaInterview";
 // react-router-dom
@@ -11,9 +9,7 @@ const MainRoute = () => {
   return (
     <Router basename = '/demo'>
         <Switch> 
-            <Route exact path="/editing" component={QuotaEditing}/>
             <Route exact path="/exceeded" component={QuotaExceeded}/>
-            <Route exact path="/tracking" component={QuotaTracking} />
             <Route exact path="/interview" component={QuotaInterview} />
             <Route exact path="/" component={QuotaManagement} />
         </Switch>
