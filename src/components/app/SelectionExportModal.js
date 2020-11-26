feef658079fd0ca703aae2daeb3cb2a4c348bc8a
Dev03-vinamr <73330185+Dev03-vinamr@users.noreply.github.com>
2020-11-26 16:42:03 +0700

    Create export data modal

--- src/components/app/SelectionExportModal.js
+++ src/components/app/SelectionExportModal.js
@@ -5,17 +5,64 @@
  * Date: 26/11/2020 
  */
 
-import React from 'react';
-// import Modal from 'react-modal';
+import React,{useEffect} from 'react';
+import Modal from 'react-modal';
 import "./styles/SelectionExportModal.css";
 
 const SelectionExportModal  = (props) =>{
+    useEffect(() =>{
+        Modal.setAppElement('body');
+    },[])
     return(
-        <Modal isOpen ={props.isOpen} className="module4--selection-export-modal">
+            <div className=".module-4--selection-export-modal-div">
+                 <Modal isOpen ={props.isOpen} className="module-4--selection-export-modal">
+            <h2>
+                Export Data
+            </h2>
+            <text className="module-4--Please-select">Please select output format </text>
             <div>
-                hi 
+            <select className="module-4--export-options">
+                <option value="first_option">MS Excel (*.xlsx)</option>
+                <option value="second_option">MS Excel (*.csv)</option>
+                <option value="third_option">SPSS (*.sav)</option>
+                <option value="forth_option" selected>PDF (*.pdf)</option>
+            </select>
             </div>
+            <text className="module-4--Please-select">Export data for</text>
+            <div>
+                <input type="checkbox" className="module-4--export-modal--checkbox"/>
+                <span>Quota Counted Interviews</span>
+            </div>
+            <div>
+                <input type="checkbox" className="module-4--export-modal--checkbox"/>
+                <span>All data</span>
+            </div>
+            <div>
+                <input type="checkbox" className="module-4--export-modal--checkbox"/>
+                <span>Cancelled Interviews</span>
+            </div>
+            <div>
+                <input type="checkbox" className="module-4--export-modal--checkbox"/>
+                <span>Selected Interviews</span>
+            </div>
+            <div>
+                <input type="checkbox" className="module-4--export-modal--checkbox"/>
+                <span>Not Completed Interviews</span>
+            </div>
+           <button className="module-4--selection-exp-modal--export-button">
+               Export
+           </button>
+             <button 
+             onClick ={props. closeExpModal}
+             >
+              Cancel
+           </button>
+                    
         </Modal>
+            </div>
+         
+  
+      
        
     )
 }
--- src/pages/app/module_4/RawData.js
+++ src/pages/app/module_4/RawData.js
@@ -22,7 +22,7 @@ const RawData = (props) => {
   const [dataRawCheck, getDataRawCheck] = useState([]);
   const [selectedCounted, setSelectedCounted] = useState();
   const [selectedCancel, setSelectedCancel] = useState();
-
+  const [isOpenExpModal,setIsOpenExpModal] = useState(false);
   /**
    *@summary Function useEffect
    *@return void
@@ -30,7 +30,16 @@ const RawData = (props) => {
   useEffect(() => {
     getRawData("0558");
   }, []);
-
+  /**
+   * 
+   * @summary Handle open and close modal 
+   */
+  const closeExpModal =() =>{
+    setIsOpenExpModal(false);
+  }
+  const openExpModal =() =>{
+    setIsOpenExpModal(true);
+  }
   /**
    *@summary The function getData for module_4
    *@param projectId
@@ -84,7 +93,7 @@ const RawData = (props) => {
           </p>
         </div>
         <div className="buttons-4">
-          <div className="button-1">Export Data</div>
+          <div className="button-1" onClick={openExpModal}>Export Data</div>
         </div>
         <div className="content-area">
           <div className="item-1">
@@ -123,7 +132,9 @@ const RawData = (props) => {
           </div>
         </div>
       </div>
-      <SelectionExportModal isOpen={true}>
+      <SelectionExportModal isOpen={isOpenExpModal} 
+      closeExpModal={closeExpModal}
+      >
 
       </SelectionExportModal>
 
