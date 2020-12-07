94b2cfc8283ca12e00bd08561ed942f639a93342
Tran Khai Hoan <hoan.tran@vinamr.local>
2020-12-07 17:00:58 +0700

    update backend module tracking

--- src/backend/controller/module4/rawDataCheck/rawDataCheck.js
+++ src/backend/controller/module4/rawDataCheck/rawDataCheck.js
@@ -8,9 +8,18 @@ module.exports.getData = async (req, res, next) => {
         //Query dữ liệu theo projectID
         let query = "SELECT * FROM `sys`.`temp` WHERE projectID=?"
         let result = await sqlQuery(connection, query, [req.query.projectId])
-        console.log(result)
         //sort result lấy các interview với stt mới nhất
+        let statusQuery = "SELECT `status`, `step`, `type`, `interviewStatus` FROM `sys`.`qctask` WHERE projectid=? AND interviewID=? ORDER BY stt DESC LIMIT 1"
         let response = sortData(result)
+        for (let i = 0; i < 1; i++) {            
+            let status = await sqlQuery(connection, statusQuery, [req.query.projectId, response[i].interviewid])
+            status = status[0]
+            for(let eachStatusProp in status) {
+                response[i][eachStatusProp] = status[eachStatusProp] 
+            }
+            console.log(response[i])
+        }
+
         connection.end()
         return res.send(response)
     }
@@ -24,6 +33,8 @@ module.exports.getData = async (req, res, next) => {
 
 module.exports.changeStatus = async (req, res, next) => {
     try {
+        let projectId = req.query.projectId
+        let interviewId = req.query.interviewId
         let connection = await dbConnection()
         if (req.query.projectId === undefined) {
             return res.status(404).json({
@@ -35,24 +46,33 @@ module.exports.changeStatus = async (req, res, next) => {
                 error: "unknown interviewId"
             })
         }
-        //Lấy interview theo projectid và interviewid mới nhất
-        let query = "SELECT * FROM `sys`.`temp` WHERE projectid=? AND interviewid=? ORDER BY stt DESC LIMIT 1"
-        let result = await sqlQuery(connection, query, [req.query.projectId, req.query.interviewId])
-        console.log(result)
-        //result trả một Array nên lấy phần tử đầu tiên
-        let changeRow = result[0]
-        //thêm mới nên stt +1
-        changeRow.stt += 1
-        changeRow.complete = req.body.status
-        console.log(changeRow)
-        //lấy từng giá trị trong result để đẩy vào làm queryVariables.
+        let dataQuery = "SELECT * FROM `sys`.`qctask` WHERE projectid=? AND interviewID=? ORDER BY stt DESC LIMIT 1"
+        let data = await sqlQuery(connection, dataQuery, [projectId, interviewId])
+        let sttQuery = "SELECT stt FROM `sys`.`qctask` ORDER BY stt DESC LIMIT 1"
+        let newestStt = await sqlQuery(connection, sttQuery)
+        newestStt = newestStt[0]
+        console.log(newestStt)
+        data = data[0]
+        console.log(data)
+        data.status = req.query.status
+        data.step = req.query.step
+        data.type = req.query.type
+        data.interviewStatus = req.query.interviewStatus
+        data.stt = newestStt.stt +1
         let queryVariables = []
-        for (let prop in changeRow) {
-            queryVariables.push(changeRow[prop])
+        for (let prop in data) {
+            if (data[prop] == undefined || data[prop] == null) {
+                data[prop] = " "
+            }
+            queryVariables.push(data[prop])
         }
-        let insertQuery = "INSERT INTO `sys`.`temp` VALUES(?)"
-        let insertResult = await sqlQuery(connection, insertQuery, [queryVariables])
-        return res.send(insertResult)
+        console.log(queryVariables)
+        console.log(queryVariables)
+        let statusUpdateQuery = "INSERT INTO `sys`.`qctask` VALUES (?)"
+        let updatedStatus = await sqlQuery(connection, statusUpdateQuery, [queryVariables])
+        return res.status(200).json({
+            message: "insert successfully"
+        })
     }
     catch (error) {
         console.log(error)
